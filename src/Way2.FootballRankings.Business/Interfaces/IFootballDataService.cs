2a9999095834853ebe20dad3f712072c1cbb1b1f
f3llps <fellipeversiani@gmail.com>
2019-05-18 03:45:00 -0300

    Criar interface do serviço FootballDataService

---
+++ src/Way2.FootballRankings.Business/Interfaces/IFootballDataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Way2.FootballRankings.Business.Models;
+
+namespace Way2.FootballRankings.Business.Interfaces
+{
+    public interface IFootballDataService
+    {
+         Task<HttpResponseMessage>  ObterTodasCompeticoesAsync();
+    }
+}
