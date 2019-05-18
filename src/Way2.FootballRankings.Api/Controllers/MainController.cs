b0d07e125511c9da8d61812c57c3bbafb9d7e0c2
f3llps <fellipeversiani@gmail.com>
2019-05-18 14:37:04 -0300

    Criar uma classe abstrata para validações genéricas

--- src/Way2.FootballRankings.Api/Controllers/MainController.cs
+++ src/Way2.FootballRankings.Api/Controllers/MainController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Way2.FootballRankings.Api.ViewModels;
 using Way2.FootballRankings.Business.Interfaces;
@@ -12,21 +13,4 @@ namespace Way2.FootballRankings.Api.Controllers
 
     }
 
-    [Route("api/competitions")]
-    public class CompetitionController : MainController
-    {
-        private readonly IFootballDataService _footballDataService;
-
-        CompetitionController(IFootballDataService footballDataService)
-        {
-            _footballDataService = footballDataService;
-        }
-
-        public async Task<ActionResult<IEnumerable<CompetitionViewModel>>> ObterTodasCompeticoes()
-        {
-            var competicoes = await _footballDataService.ObterTodasCompeticoesAsync();
-
-            return Ok(competicoes);
-        }
-    }
 }
