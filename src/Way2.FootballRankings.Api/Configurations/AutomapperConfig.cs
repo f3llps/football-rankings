13155cbdbeb18f4cbe35bf4faaf9329b33a321e6
f3llps <fellipeversiani@gmail.com>
2019-05-18 14:36:31 -0300

    Configurar Autommaper

---
+++ src/Way2.FootballRankings.Api/Configurations/AutomapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Way2.FootballRankings.Api.ViewModels;
+using Way2.FootballRankings.Business.Models;
+
+namespace Way2.FootballRankings.Api.Configurations
+{
+    public class AutomapperConfig : Profile
+    {
+        public AutomapperConfig()
+        {
+            CreateMap<Competition, CompetitionViewModel>().ReverseMap();
+            CreateMap<TeamStandings, TeamStandingsViewModel>().ReverseMap();
+            CreateMap<Area, AreaViewModel>().ReverseMap();
+            CreateMap<Standings, StandingsViewModel>().ReverseMap();
+            CreateMap<TeamStandings, TeamStandingsViewModel>().ReverseMap();
+        }
+    }
+}
