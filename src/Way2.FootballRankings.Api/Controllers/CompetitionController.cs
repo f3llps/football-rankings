62f4552a44c94ef82ea6d1de7aa799fb4f841318
f3llps <fellipeversiani@gmail.com>
2019-05-18 14:37:46 -0300

    Criar um Controller específico para competições

---
+++ src/Way2.FootballRankings.Api/Controllers/CompetitionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Way2.FootballRankings.Api.ViewModels;
+using Way2.FootballRankings.Business.Interfaces;
+
+namespace Way2.FootballRankings.Api.Controllers
+{
+    [Route("api/competitions")]
+    public class CompetitionController : MainController
+    {
+        private readonly IFootballDataService _footballDataService;
+        private readonly IMapper _mapper;
+
+        public CompetitionController(IFootballDataService footballDataService, IMapper mapper)
+        {
+            _footballDataService = footballDataService;
+            _mapper = mapper;
+        }
+
+        public async Task<ActionResult<IEnumerable<CompetitionViewModel>>> ObterTodasCompeticoes()
+        {
+            var competicoes = _mapper.Map<IEnumerable<CompetitionViewModel>>(await _footballDataService.ObterTodasCompeticoes());
+
+            return Ok(competicoes);
+        }
+    }
+}
