54afa1d61bd2c6d4d29f276450de890b8d26d784
f3llps <fellipeversiani@gmail.com>
2019-05-18 03:44:19 -0300

    Gerar um MainController para realizar validações genéricas

---
+++ src/Way2.FootballRankings.Api/Controllers/MainController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Way2.FootballRankings.Api.ViewModels;
+using Way2.FootballRankings.Business.Interfaces;
+
+namespace Way2.FootballRankings.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+
+    }
+
+    [Route("api/competitions")]
+    public class CompetitionController : MainController
+    {
+        private readonly IFootballDataService _footballDataService;
+
+        CompetitionController(IFootballDataService footballDataService)
+        {
+            _footballDataService = footballDataService;
+        }
+
+        public async Task<ActionResult<IEnumerable<CompetitionViewModel>>> ObterTodasCompeticoes()
+        {
+            var competicoes = await _footballDataService.ObterTodasCompeticoesAsync();
+
+            return Ok(competicoes);
+        }
+    }
+}
