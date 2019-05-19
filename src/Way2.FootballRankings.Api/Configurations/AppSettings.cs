8e6e8114c87b30f79ee51b04b9e162ecffef2b08
f3llps <fellipeversiani@gmail.com>
2019-05-19 08:25:20 -0300

    Obter Token do appsettings.json

---
+++ src/Way2.FootballRankings.Api/Configurations/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Way2.FootballRankings.Api.Configurations
+{
+    public class AppSettings
+    {
+        public string FootballDataService { get; set; }
+    }
+
+    //public class AppSettings
+    //{
+    //    public Tokens Tokens { get; set; }
+    //}
+}
--- src/Way2.FootballRankings.Api/Startup.cs
+++ src/Way2.FootballRankings.Api/Startup.cs
@@ -10,12 +10,16 @@ namespace Way2.FootballRankings.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
