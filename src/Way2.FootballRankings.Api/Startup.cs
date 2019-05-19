d199b6fb9e0c5ad6181f0ddb09f522f65a1d818a
f3llps <fellipeversiani@gmail.com>
2019-05-19 13:13:11 -0300

    Incorporar documentação via Swagger na API

--- src/Way2.FootballRankings.Api/Startup.cs
+++ src/Way2.FootballRankings.Api/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Way2.FootballRankings.Api.Configurations;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Way2.FootballRankings.Api
 {
@@ -16,7 +17,27 @@ namespace Way2.FootballRankings.Api
             services.AddAutoMapper(typeof(Startup));
             services.AddMemoryCache();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1",
+                new Info
+                {
+                    Title = "Documentação da API FootballRankingks",
+                    Description = "A API FootballRankingks foi desenvolvida como teste técnico para vaga desenvolvedor FullStack da Way2",
+                    Version = "v1.0",
+                    Contact = new Contact
+                    {
+                        Name = "Fellipe Gomes Versiani",
+                        Email = "fellipeversiani@gmail.com",
+                        Url = "https://www.linkedin.com/in/f3llps/"
+                    }
+                });
+            });
+
+
             services.ResolveDependencies();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,8 +53,14 @@ namespace Way2.FootballRankings.Api
                 app.UseHsts();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "API FootballRankings");
+            });
+
             app.UseHttpsRedirection();
             app.UseMvc();
         }
-    }
+}
 }
